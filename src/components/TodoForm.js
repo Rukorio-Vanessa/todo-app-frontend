1f4d11dca4e3fb89ec048df0a342409f4f0dfb58
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-27 00:22:06 +0300

    added functionality to form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,30 +1,57 @@
 import React from "react";
+import { useState } from "react";
 
-function TodoForm(){
+const initialState = {
+    title: "",
+    description: "",
+    status: "",
+    priority: "",
+};
+
+function TodoForm({onAddTodo}){
+    const [formData, setFormData] = useState(initialState);
+    
+
+    function handleChange(e) {
+        setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+        });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(formData)
+    }
+      
     return(
         <div className="card">
         <h2>Add Todo</h2>
-        <form >
-          <label >Title: </label>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Title: </label>
           <input
             type="text"
             id="title"
+            value={formData.title}
+            onChange={handleChange}
           />
-          <label>Description: </label>
+          <label htmlFor="description">Description: </label>
           <input
             type="text"
-            id="image"
+            id="description"
+            value={formData.description}
+            onChange={handleChange}
           />
-          <label>Status: </label>
-          <select className="form-select">
+          <label htmlFor="status">Status: </label>
+          <select className="form-select" id="status" value={formData.status} onChange={handleChange}>
                <option>Select</option>
-               <option value={0}>CREATED</option>
+               <option value={0}>STARTED</option>
                <option value={1}>IN PROGRESS</option>
                <option value={2}>COMPLETED</option>
                <option value={3}>CANCELLED</option>
           </select>
-          <label>Priority: </label>
-          <select className="form-select"htmlFor="description" >
+          <label htmlFor="priority">Priority: </label>
+          <select className="form-select" id="priority" value={formData.priority} onChange={handleChange} >
                <option>Select</option>
                <option value={0}>LOW</option>
                <option value={1}>MEDIUM</option>
