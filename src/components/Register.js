18145b958a1b88e26e83774380de9bef2d1710a6
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-27 02:58:18 +0300

    created a refister form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,33 @@
+import React from "react";
+
 function Register(){
     return(
-        Register
+        <div className="register-card">
+        <form >
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+        />
+        <label htmlFor="username">Username:</label>
+        <input
+          type="email"
+          id="email"
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+        />
+        <label htmlFor="confirm-password">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirm-password"
+        />
+        <button type="submit">Register</button>
+      </form>
+      </div>
     )
 }
+
+export default Register;
