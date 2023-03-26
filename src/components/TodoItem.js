2390f8ba67ff4fffdf8395b7065faf9ffd87e766
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-27 01:55:08 +0300

    modified display table code

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,8 +1,54 @@
 import React from "react";
+import { useState } from "react";
 
 function TodoItem({spice}){
 
-    
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            title: 'Code Challenge',
+            description: 'Complete coding challenge',
+            status: 'IN PROGRESS',
+            priority: 'HIGH'
+        },
+        {
+            id: 2,
+            title: 'Online Lecture',
+            description: 'Attend online lecture on serialization in rails',
+            status: 'COMPLETED',
+            priority: 'HIGH'
+        },
+        {
+            id: 3,
+            title: 'Personal website',
+            description: 'Create a personal website to showcase coding projects',
+            status: 'CREATED',
+            priority: 'MEDIUM'
+        },
+        {
+            id: 4,
+            title: 'Review and Debug',
+            description: 'Review and debug code for group project',
+            status: 'IN PROGRESS',
+            priority: 'LOW'
+        },
+        {
+            id: 5,
+            title: 'Virtual Conference',
+            description: 'Attend virtual tech conference',
+            status: 'CANCELLED',
+            priority: 'LOW'
+        },
+        {
+            id: 6,
+            title: 'Virtual Conference',
+            description: 'Attend virtual tech conference',
+            status: 'CANCELLED',
+            priority: 'LOW'
+        }
+    ]);
+
+   
     return(
         <table className="todo-table">
             <thead>
@@ -16,76 +62,22 @@ function TodoItem({spice}){
 
             </thead>
             <tbody>
-                <tr>
-                    <td>Code Challenge</td>
-                    <td>Complete coding challenge</td>
-                    <td>IN PROGRESS</td>
-                    <td>HIGH</td>
-                    <td className="row">
-                        <div className="col-6">
-                            <button className="btn btn-info">Update</button>
-                        </div>
-                        <div className="col-6">
-                            <button className="btn btn-danger">Delete</button>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Online Lecture</td>
-                    <td>Attend online lecture on serialization in rails</td>
-                    <td>COMPLETED</td>
-                    <td>HIGH</td>
-                    <td className="row">
-                        <div className="col-6">
-                            <button className="btn btn-info">Update</button>
-                        </div>
-                        <div className="col-6">
-                            <button className="btn btn-danger">Delete</button>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Personal website</td>
-                    <td>Create a personal website to showcase coding projects</td>
-                    <td>CREATED</td>
-                    <td>MEDIUM</td>
-                    <td className="row">
-                        <div className="col-6">
-                            <button className="btn btn-info">Update</button>
-                        </div>
-                        <div className="col-6">
-                            <button className="btn btn-danger">Delete</button>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Review and Debug</td>
-                    <td>Review and debug code for group project</td>
-                    <td>IN PROGRESS</td>
-                    <td>LOW</td>
-                    <td className="row">
-                        <div className="col-6">
-                            <button className="btn btn-info">Update</button>
-                        </div>
-                        <div className="col-6">
-                            <button className="btn btn-danger">Delete</button>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Virtual Conference</td>
-                    <td>Attend virtual tech conference </td>
-                    <td>CANCELLED</td>
-                    <td>LOW</td>
-                    <td className="row">
-                        <div className="col-6">
-                            <button className="btn btn-info">Update</button>
-                        </div>
-                        <div className="col-6">
-                            <button className="btn btn-danger">Delete</button>
-                        </div>
-                    </td>
-                </tr>
+            {todos.map((todo) => (
+                    <tr key={todo.id}>
+                        <td>{todo.title}</td>
+                        <td>{todo.description}</td>
+                        <td>{todo.status}</td>
+                        <td>{todo.priority}</td>
+                        <td className="row">
+                            <div className="col-6">
+                                <button className="btn btn-info">Update</button>
+                            </div>
+                            <div className="col-6">
+                                <button className="btn btn-danger" >Delete</button>
+                            </div>
+                        </td>
+                    </tr>
+                ))}
             </tbody>
         </table>
       )
