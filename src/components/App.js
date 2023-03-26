b0f9bfc5610a3c41493a5e38f6ef9174fbf61d9d
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-26 22:53:10 +0300

    created for layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import '../App.css';
 import Heading from './Heading';
+import TodoForm from './TodoForm';
 
 function App() {
   return (
    <>
    <Heading/>
+   <main>
+    <TodoForm/>
+   </main>
    </>
   );
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,40 @@
+import React from "react";
+
 function TodoForm(){
     return(
-        TodoForm
+        <div className="card">
+        <h2>Add Todo</h2>
+        <form >
+          <label >Title: </label>
+          <input
+            type="text"
+            id="title"
+          />
+          <label>Description: </label>
+          <input
+            type="text"
+            id="image"
+          />
+          <label>Status: </label>
+          <select className="form-select">
+               <option>Select</option>
+               <option value={0}>CREATED</option>
+               <option value={1}>IN PROGRESS</option>
+               <option value={2}>COMPLETED</option>
+               <option value={3}>CANCELLED</option>
+          </select>
+          <label>Priority: </label>
+          <select className="form-select"htmlFor="description" >
+               <option>Select</option>
+               <option value={0}>LOW</option>
+               <option value={1}>MEDIUM</option>
+               <option value={2}>HIGH</option>
+          </select>
+          
+          <button type="submit">Add to List</button>
+        </form>
+      </div>
     )
 }
+
+export default TodoForm;
