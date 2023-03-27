60e84e74a4a090d587e448bafb6a715b9c05067c
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-27 04:05:34 +0300

    added some functionality to register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,28 +1,52 @@
 import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 function Register(){
+
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const history = useHistory();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        history.push("/login");
+
+    };
+    
     return(
         <div className="register-card">
-        <form >
+        <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
         <input
           type="email"
           id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
         <label htmlFor="username">Username:</label>
         <input
-          type="email"
-          id="email"
+          type="text"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
         />
         <label htmlFor="password">Password:</label>
         <input
           type="password"
           id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
         <label htmlFor="confirm-password">Confirm Password:</label>
         <input
           type="password"
           id="confirm-password"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
         />
         <button type="submit">Register</button>
       </form>
